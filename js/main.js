1c30235bde44e9d103048ff3fa63a589d7a96673
JDelPuerto <jdelpuerto@codium.com.py>
2019-10-13 23:25:38 -0300

    Se agregan mas comentarios al codigo fuente

--- js/main.js
+++ js/main.js
@@ -73,27 +73,44 @@
         return obj;
     }
 
+    /**
+     * 
+     * @param {*} numeroDecimal  el digito a ser traducido
+     * @returns un objeto compuesto de dos atributos: La longitud del numero decimal y el numero romano equivalente
+     */
     function R(numeroDecimal) {
+
+        // Caso base de la recursion
         if (match(numeroDecimal, "eof")) {
             return crearResultado(0, "");
         }
         else {
+            //Se  calcula el numero romano equivalente de los digitos a la derecha del digito actual
             var RAnterior = R(numeroDecimal.slice(1, numeroDecimal.length));
         }
 
+        //Esta variable representa la posicion del digito actualmente evaluado
         var RxAcutal = RAnterior.Rx + 1;
 
+        //Esta variable representa el numero romano equivalente SOLO del digito actual segun su posicion en el numero decimal
         var RyActual = transformar(numeroDecimal.slice(0, 1), RxAcutal);
 
         //Verifica que no se acepten numeros del 101 al 199
-        if (match(RyActual, "C") &&  !match(RAnterior.Ry, "") ) throw new Error("Numero no soportado");
+        if (match(RyActual, "C") && !match(RAnterior.Ry, "")) throw new Error("Numero no soportado");
 
-        return crearResultado(RxAcutal,   RyActual + RAnterior.Ry);
+        //Se concatena el numero romano equivalente del digito actual junto al numero romano equivalente de los digitos mas a la derecha del mismo
+        return crearResultado(RxAcutal, RyActual + RAnterior.Ry);
     }
 
 
+    /**
+     * 
+     * @param {*} input el digito a ser comparado
+     * @param {*} Rx representa la posicion del digito en el numero decimal
+     * @returns una cadena que representa al digito decimal segun su posicion
+     */
     function transformar(input, Rx) {
-        // Establece los valores de salida  para el digito de la unidad
+        // Establece los valores de salida para cada caso del digito de la unidad
         if (Rx == 1) {
             if (match(input, 0)) return "";
 
@@ -115,7 +132,7 @@
 
             if (match(input, 9)) return "IX";
         }
-        // Establece los valores de salida  para el digito de la decena
+        // Establece los valores de salida para cada caso del digito de la decena
         else if (Rx == 2) {
             if (match(input, 0)) return "";
 
@@ -137,7 +154,7 @@
 
             if (match(input, 9)) return "XC";
         }
-         // Establece los valores de salida  para el digito de la centena
+        // Establece los valores de salida para cada caso del digito de la centena
         else if (Rx == 3) {
 
             if (match(input, 0)) return "";
@@ -150,7 +167,7 @@
         }
         else {
 
-             //Verifica que no se acepten numeros mayores a  1000
+            //Verifica que no se acepten numeros mayores a  1000
             throw new Error("Numero no soportado");
         }
 
@@ -158,7 +175,12 @@
     }
 
 
-    //Funcion match que se encarga de comparar caracteres
+    /**
+     * 
+     * @param {*} input el caracter leido de la entrada a ser comparado 
+     * @param {*} caracter el caracter conocido por la gramatica
+     * @returns true si los caracteres coinciden en caso contrario false
+     */
     function match(input, caracter) {
         if (input == caracter) {
             return true;
